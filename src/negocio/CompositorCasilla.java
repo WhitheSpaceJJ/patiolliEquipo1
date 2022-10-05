53f77b95611125c0f65a9aa483857aa39554aa00
Jose\Jose <jose.orozco229141@potros.itson.edu.mx>
2022-10-04 18:22:44 -0700

    nuevas figuras

---
+++ src/negocio/CompositorCasilla.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negocio;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Judi
+ */
+public class CompositorCasilla extends Figura {
+
+    private java.util.List< Figura> lista = new ArrayList<>();
+
+    public CompositorCasilla(String posicion) {
+        super(posicion);
+    }
+
+    @Override
+    public void dibujar() {
+        for (int i = 0; i < lista.size(); i++) {
+            Figura get = lista.get(i);
+            get.dibujar();
+        }
+    }
+
+    public void addElemento(Figura figura) {
+        this.lista.add(figura);
+    }
+
+    public void removeElemento(Figura figura) {
+        this.lista.remove(figura);
+    }
+
+    public java.util.List<Figura> getLista() {
+        return lista;
+    }
+
+    public void setLista(java.util.List<Figura> lista) {
+        this.lista = lista;
+    }
+
+}
---
+++ src/negocio/INegocio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package negocio;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Judi
+ */
+public interface INegocio {
+
+    public void dibujarTablero(Graphics g,int tamaño);
+        public void dibujarDados(Graphics g);
+}
